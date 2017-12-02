f918c19f8b09c98c697f130bfe21f9318bf5107d
DmitryDmD <deloakk.mr@mail.ru>
2017-12-02 19:13:42 +0300

    Добавление остальных значений свойств объявлений

--- js/map.js
+++ js/map.js
@@ -1,11 +1,11 @@
 'use strict';
-
-var advertisements = [];
-var advCount = 8;
+var userAvatars = 8;
 var offerTitles = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 var placeTypes = ['flat', 'house', 'bungalo'];
+var checkInTimeMarks = ['12:00', '13:00', '14:00'];
+var checkOutTimeMarks = ['12:00', '13:00', '14:00'];
 
-var randomLocationCoordinates = function (start, end) {
+var getRandomLocationCoordinates = function (start, end) {
   var coordinates = [];
 
   for (var i = start; i <= end; i++) {
@@ -17,52 +17,74 @@ var randomLocationCoordinates = function (start, end) {
   return result;
 };
 
-var randomPrice = function (priceStart, priceEnd) {
+var getRandomPrice = function (priceStart, priceEnd) {
   var randomNumber = Math.floor(priceStart + Math.random() * priceEnd);
   return randomNumber;
 };
 
-var randomType = function () {
+var getRandomType = function () {
   var randomNumber = Math.floor(Math.random() * placeTypes.length);
   return placeTypes[randomNumber];
 };
 
-var randomCount = function (minCount, maxCount) {
+var getRandomCheckIn = function () {
+  var randomNumber = Math.floor(Math.random() * checkInTimeMarks.length);
+  return checkInTimeMarks[randomNumber];
+}
+
+var getRandomCheckOut = function () {
+  var randomNumber = Math.floor(Math.random() * checkOutTimeMarks.length);
+  return checkOutTimeMarks[randomNumber];
+}
+
+var getRandomCount = function (minCount, maxCount) {
   var randomNumber = Math.floor(minCount + Math.random() * maxCount);
   return randomNumber;
 };
 
-for (var i = 0; i < advCount; i++) {
-  advertisements[i] = {
-    "author": {
-      "avatar": 'img/avatars/user0' + (i + 1) + '.png'
-    },
-    "offer": {
-      "title": offerTitles[i],
-      "address": 'location.x, y',
-      "price": 'Цена - ' + randomPrice(1000, 1000000),
-      "type": 'Тип помещения - ' + randomType(),
-      "rooms": 'Количество комнат - ' + randomCount(1, 5),
-      "guests": 'Количество гостей - ' + randomCount(1, 10),
-      "checkin": '12:00',
-      "checkout": '12:00',
-      "features": 'массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"',
-      "description": '',
-      "photos": []
-    },
-    "location": {
-      "x": 'Координата x - ' + randomLocationCoordinates(300, 900),
-      "y": 'Координата y - ' + randomLocationCoordinates(100, 500)
-    }
-  };
-}
+var getFeatures = function () {
+  var fixedOfferFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+  var offerFeatures = [];
+  for (var i = 0; i < 1 + Math.floor(Math.random() * fixedOfferFeatures.length); i++) {
+    offerFeatures.push(fixedOfferFeatures[i]);
+  }
+  
+  return offerFeatures;
+};
 
-console.log(advertisements[0].author.avatar);
-console.log(advertisements[0].offer.title);
-console.log(advertisements[0].offer.address);
-console.log(advertisements[0].offer.price);
-console.log(advertisements[0].offer.type);
-console.log(advertisements[0].offer.rooms);
-console.log(advertisements[0].offer.guests);
-console.log(advertisements[0].location.x);
-console.log(advertisements[0].location.y);
+var getAdvertisements = function (advCount) {
+  for (var i = 0; i < advCount; i++) {
+    var advertisements = [];
+    var locationX = getRandomLocationCoordinates(300, 900);
+    var locationY = getRandomLocationCoordinates(100, 500);
+
+    advertisements[i] = {
+      "author": {
+        "avatar": 'img/avatars/user0' + (1 + Math.floor(Math.random() * userAvatars)) + '.png'
+      },
+      "offer": {
+        "title": offerTitles[Math.floor(Math.random() * offerTitles.length)],
+        "address": 'Адрес - ' + locationX + ', ' + locationY,
+        "price": 'Цена - ' + getRandomPrice(1000, 999000),
+        "type": 'Тип помещения - ' + getRandomType(),
+        "rooms": 'Количество комнат - ' + getRandomCount(1, 5),
+        "guests": 'Количество гостей - ' + getRandomCount(1, 10),
+        "checkin": 'Время регистрации - ' + getRandomCheckIn(),
+        "checkout": 'Время выписки - ' + getRandomCheckOut(),
+        "features": 'Особенности - ' + getFeatures(),
+        "description": '',
+        "photos": []
+      },
+      "location": {
+        "x": 'Координата x - ' + locationX,
+        "y": 'Координата y - ' + locationY
+      }
+    };
+  }
+  
+  return advertisements;
+};
+
+var advertisements = getAdvertisements(8);
+
+document.querySelector('.map').classList.remove('map--faded');
