72681ad1b8c82d6957b0c66f007d4283d85b2d6d
DmitryDmD <deloakk.mr@mail.ru>
2017-12-02 18:14:47 +0300

    Добавление случайных цен и типов

--- js/map.js
+++ js/map.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var advertisements = [];
-
+var advCount = 8;
 var offerTitles = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var placeTypes = ['flat', 'house', 'bungalo'];
 
 var randomLocationX = function (start, end) {
   var coordinatesX = [];
@@ -14,7 +15,7 @@ var randomLocationX = function (start, end) {
   var result = coordinatesX.splice(Math.floor(Math.random() * coordinatesX.length), 1);
 
   return result;
-}
+};
 
 var randomLocationY = function (start, end) {
   var coordinatesY = [];
@@ -26,25 +27,39 @@ var randomLocationY = function (start, end) {
   var result = coordinatesY.splice(Math.floor(Math.random() * coordinatesY.length), 1);
   
   return result;
-}
+};
 
-for (var i = 0; i < 8; i++) {
+var priceStart = 1000;
+var priceEnd = 999000;
+
+var randomPrice = function () {
+  var randomNumber = Math.floor(priceStart + Math.random() * priceEnd);
+
+  return randomNumber;
+};
+
+var randomType = function () {
+  var randomNumber = Math.floor(Math.random() * 3);
+  return placeTypes[randomNumber];
+};
+
+for (var i = 0; i < advCount; i++) {
   advertisements[i] = {
     "author": {
       "avatar": 'img/avatars/user0' + (i + 1) + '.png'
     },
     "offer": {
       "title": offerTitles[i],
-      "address": 'location.x, location.y',
-      "price": 'от 1000 до 1 000 000',
-      "type": 'flat',
+      "address": 'location.x, y',
+      "price": randomPrice(),
+      "type": randomType(),
       "rooms": 'от 1 до 5',
       "guests": 'случайное количество гостей, которое можно разместить',
       "checkin": '12:00',
       "checkout": '12:00',
       "features": 'массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"',
       "description": '',
-      "photos": ''
+      "photos": []
     },
     "location": {
       "x": randomLocationX(300, 900),
@@ -54,7 +69,9 @@ for (var i = 0; i < 8; i++) {
 
   console.log(advertisements[i].author.avatar);
   console.log(advertisements[i].offer.title);
-  
+  console.log(advertisements[i].offer.address);
+  console.log(advertisements[i].offer.price);
+  console.log(advertisements[i].offer.type);
   console.log(advertisements[i].location.x);
   console.log(advertisements[i].location.y);
 }
