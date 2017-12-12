0e40cb611e351c852ded544845f6218426d5e2c2
DmitryDmD <deloakk.mr@mail.ru>
2017-12-12 23:08:10 +0300

    Реализация перемещения метки

--- js/map.js
+++ js/map.js
@@ -1,6 +1,9 @@
 'use strict';
 
 (function () {
+  var PIN_MAIN_HEIGHT = 44;
+  var mapMinHeight = 125;
+  var mapMaxHeight = 655;
   var mapPinMain = window.pin.mapPins.querySelector('.map__pin--main');
   var clickedElement;
 
@@ -42,4 +45,48 @@
   mapPinMain.addEventListener('mouseup', mapPinMainMouseUpHandler);
   window.pin.mapPins.addEventListener('click', mapPinClickHandler);
   window.pin.mapPins.addEventListener('keydown', mapPinKeydownHandler);
+
+  mapPinMain.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var mouseMoveHandler = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
+      if (mapPinMain.offsetTop - shift.y < mapMinHeight) {
+        mapPinMain.style.top = mapMinHeight + 'px';
+      }
+      if (mapPinMain.offsetTop - shift.y > mapMaxHeight) {
+        mapPinMain.style.top = mapMaxHeight + 'px';
+      }
+      mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
+
+      window.form.inputAddress.value = 'x: ' + (mapPinMain.offsetLeft - shift.x) + ', ' + 'y: ' + (mapPinMain.offsetTop - shift.y + PIN_MAIN_HEIGHT);
+    };
+
+    var mouseUpHandler = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      document.removeEventListener('mouseup', mouseUpHandler);
+    };
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+  });
 })();
